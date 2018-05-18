71a8234ce854c27193db097d3538b469132d2de5
reznikovkg <reznikov.kosta@yandex.ru>
2018-05-18 21:54:39 +0300

    initial commit

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private IP Prop;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Prop = new IP();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoom(0, 50);
+            chart1.ChartAreas[0].CursorX.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorX.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisX.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisX.ScrollBar.IsPositionedInside = true;
+
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoom(-1, 20);
+            chart1.ChartAreas[0].CursorY.IsUserEnabled = true;
+            chart1.ChartAreas[0].CursorY.IsUserSelectionEnabled = true;
+            chart1.ChartAreas[0].AxisY.ScaleView.Zoomable = true;
+            chart1.ChartAreas[0].AxisY.ScrollBar.IsPositionedInside = true;
+        }
+        
+        private void chart1_Click(object sender, EventArgs e)
+        {
+            chart1.Series[1].Points.AddXY(13, 13);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Myf();
+            IP Mycl = new IP();
+
+            Mycl.SetA(14.3);
+            chart1.Series[1].Points.AddXY(3, Mycl.GetA());
+        }
+
+        public void Myf()
+        {
+
+            for (int i = (int)Prop.GetA(); i < (int)Prop.GetB(); i += 1)
+            {
+                chart1.Series[0].Points.AddXY(i, Math.Cos(i));
+            }
+            for (int i = 0; i < 50; i += 3)
+            {
+                chart1.Series[1].Points.AddXY(i, Math.Sin(i));
+            }
+        }
+
+        private void fontDialog1_Apply(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_rebuild(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.AddXY(15, 20);
+        }
+    }
+}
---
+++ WindowsFormsApp1/IP.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    class IP
+    {
+        private double a;
+        private double b;
+
+        
+        public IP()
+        {
+            this.a = 3;
+            this.b = 12;
+        }
+
+        public double f(double x)
+        {
+            return Math.Cos(x);
+        }
+
+        ~IP()
+        {
+
+        }
+
+        public void SetA(double _a)
+        {
+            this.a = _a;
+        }
+
+        public double GetA()
+        {
+            return this.a;
+        }
+
+
+        public void SetB(double _b)
+        {
+            this.b = _b;
+        }
+
+        public double GetB()
+        {
+            return this.b;
+        }
+    }
+}
